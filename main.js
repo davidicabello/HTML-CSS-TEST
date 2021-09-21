2c74d2f6fd30aaf00cfe777f1231d76408c6e79b
davidlp90 <davidivan.eda@gmail.com>
2021-09-21 15:10:24 -0300

    updates
    updates

--- main.js
+++ main.js
@@ -13,3 +13,31 @@ typewriter.typeString('Developer')
     .typeString('Freelancer')
     .pauseFor(2500)
     .start();
+
+var app = document.getElementById('app2');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Mis habilidades:')
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString('HTML5')
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString('CSS3')
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString('JavaScript')
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString('Visual Studio Code')
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString('GitHub')
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString('git')
+    .pauseFor(1000)
+    .start();
